a23c431515c6e5b1a03367c77964dff75d5bca34
malachi <lyle.hart.3@gmail.com>
2016-02-27 12:50:16 -0500

    working with modules in separate files, this also teaches me what I need to know for lyle1

--- js/studentController.js
+++ js/studentController.js
@@ -1,13 +1,19 @@
-var mainApp = angular.module("mainApp", []);
-
-mainApp.controller("studentController",
-	function ($scope) {
-		$scope.student = {
-			firstName: "Mahesh",
-			lastName: "Parashar",
-			fullName: function() {
-				return this.firstName + " " + this.lastName;
-			}
-		}
-	});
-
+mainApp.controller("studentController", function($scope) {
+   $scope.student = {
+      firstName: "Mahesh",
+      lastName: "Parashar",
+      fees:500,
+      
+      subjects:[
+         {name:'Physics',marks:70},
+         {name:'Chemistry',marks:80},
+         {name:'Math',marks:65},
+         {name:'English',marks:75},
+         {name:'Hindi',marks:67}
+      ],
+      
+      fullName: function() {
+         return this.firstName + " " + this.lastName;
+      }
+   };
+});
